5f50ce62cd5944759a517864bf7df52e065cfc26
balatskiyart <balatskiyart@outlook.com>
2016-03-07 20:32:48 -0800

    Complete Homework

---
+++ Java_HW_31/src/main/java/core/QA1.java
@@ -0,0 +1,26 @@
+package core;
+
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+public class QA1 {
+    public static void main(String[] args) {
+        FirefoxDriver driver = new FirefoxDriver();   //Version 1.1 :: Firefox
+        // WebDriver driver = new HtmlUnitDriver(); // Version 1.2 :: HtmlUnit
+        String url = "http://learn2test.net";
+        String title_expected = "learn2test.net";
+        driver.get(url);
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        String title_actual = driver.getTitle();
+
+        if (title_expected.equals(title_actual)){
+            System.out.println("PASSED");
+        } else {
+            System.out.println("FAILED");
+
+        }
+        driver.quit();
+    }
+}
